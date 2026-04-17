2c82d85c335a3836f6473cae0028692e01e5aa62
Kim Boender <kim@metgrace.nl>
2026-04-17 22:28:12 +0200

    feat: integrate Umami analytics script into default.vue and remove from nuxt.config.ts

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+    useHead({
+        script: [
+            {
+                defer: true,
+                src: "https://cloud.umami.is/script.js",
+                "data-website-id": "2cf56603-9061-42b6-982f-a3683778149e",
+            },
+        ],
+    });
 </script>
 
 <template>
